120734512f70c6b05ea6f64f266a974c440f0e79
Pedro Levandowski <146142910+Pedro-Levandowski@users.noreply.github.com>
2026-09-01 17:58:08 -0300

    feat: Implementa conversão de expressão infixa para RPN

--- src/ConversorRPN.java
+++ src/ConversorRPN.java
@@ -8,7 +8,58 @@ public class ConversorRPN {
         List<String> saida = new ArrayList<>();
         Stack<Character> operadores = new Stack<>();
 
-        return "";
+        StringBuilder numero = new StringBuilder();
+
+        for (int i = 0; i < expressao.length(); i++) {
+            char caractere = expressao.charAt(i);
+
+            if (Character.isDigit(caractere) || caractere == '.') {
+                numero.append(caractere);
+            } else {
+                if (!numero.isEmpty()) {
+                    saida.add(numero.toString());
+                    numero.setLength(0);
+                }
+
+                if (Character.isWhitespace(caractere)) {
+                    continue;
+                }
+
+                if (caractere == '(') {
+                    operadores.push(caractere);
+                } else if (caractere == ')') {
+                    while (!operadores.isEmpty()
+                            && operadores.peek() != '(') {
+                        saida.add(String.valueOf(operadores.pop()));
+                    }
+
+                    if (!operadores.isEmpty()
+                            && operadores.peek() == '(') {
+                        operadores.pop();
+                    }
+                } else if (ehOperador(caractere)) {
+                    while (!operadores.isEmpty()
+                            && operadores.peek() != '('
+                            && precedencia(operadores.peek()) >= precedencia(caractere)) {
+
+                        saida.add(String.valueOf(operadores.pop()));
+                    }
+
+                    operadores.push(caractere);
+                }
+            }
+        }
+
+        if (!numero.isEmpty()) {
+            saida.add(numero.toString());
+        }
+
+        while (!operadores.isEmpty()) {
+            saida.add(String.valueOf(operadores.pop()));
+        }
+
+        return String.join(" ", saida);
+
     }
 
     private static int precedencia(char operador) {
@@ -23,7 +74,7 @@ public class ConversorRPN {
         return 0;
     }
 
-    private static boolean ehOperdor(char caractere) {
+    private static boolean ehOperador(char caractere) {
         return caractere == '+'
                 || caractere == '-'
                 || caractere == '*'
