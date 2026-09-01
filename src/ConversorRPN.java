20355ceefeddd455e0f51b7953dc82ea2e6a2c00
Pedro Levandowski <146142910+Pedro-Levandowski@users.noreply.github.com>
2026-09-01 17:33:44 -0300

    feat: Adiciona identificação e precedencia de operadores

--- src/ConversorRPN.java
+++ src/ConversorRPN.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class ConversorRPN {
+
+    public static String converter(String expressao) {
+        List<String> saida = new ArrayList<>();
+        Stack<Character> operadores = new Stack<>();
+
+        return "";
+    }
+
+    private static int precedencia(char operador) {
+        if(operador == '+' || operador == '-') {
+            return 1;
+        }
+
+        if(operador == '*' || operador == '/') {
+            return 2;
+        }
+
+        return 0;
+    }
+
+    private static boolean ehOperdor(char caractere) {
+        return caractere == '+'
+                || caractere == '-'
+                || caractere == '*'
+                || caractere == '/';
+    }
 }
